650e6c72ed7474ffc2c6769efe53e2c1d4fb04c5
qegabbe <qegabbe@outlook.de>
2016-02-23 01:03:21 +0100

    Add Robot to control keys from Authentication

--- src/test/java/com/lazerycode/selenium/tests/AppTest.java
+++ src/test/java/com/lazerycode/selenium/tests/AppTest.java
@@ -4,12 +4,20 @@ import com.lazerycode.selenium.DriverFactory;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+// TestNG packages
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
+// selenium packages
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
+import org.openqa.selenium.Alert;
+// robot packages
+import java.awt.*;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 public class AppTest {
   private WebDriver driver;
@@ -24,31 +32,70 @@ public class AppTest {
 			  "D://EclipseWS//maven-template//selenium_standalone//windows//googlechrome//32bit//chromedriver.exe");
 	driver = new ChromeDriver();
     baseUrl = "https://tb.itgain.de/";
+    driver.get(baseUrl + "/pendulo/");
     
-    //String currentWindow = driver.getWindowHandle();
-    // Switch to Authentication /Required Frame
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    // tell selenium to wait maximum 10 seconds for searching elements
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    Thread.sleep(4000);
     //
     for ( String currentwindow : driver.getWindowHandles()) {
     	System.out.println("CurrentWindow>:" + currentwindow);
     }
     //
+    try {
+        // Switch to JavaScript Alert window    	
+        Robot robot = new Robot();
+        // insert username: haw
+        Thread.sleep(1000);
+        robot.keyPress(KeyEvent.VK_H);
+        robot.keyPress(KeyEvent.VK_W);        
+        robot.keyPress(KeyEvent.VK_A);
+        //
+        robot.keyPress(KeyEvent.VK_TAB);//go to password feild
+        Thread.sleep(1000);
+        // insert password: Meshmesh90!
+        robot.keyPress(KeyEvent.VK_M);
+        robot.keyPress(KeyEvent.VK_E);
+        robot.keyPress(KeyEvent.VK_S);
+        robot.keyPress(KeyEvent.VK_H);
+        robot.keyPress(KeyEvent.VK_M);
+        robot.keyPress(KeyEvent.VK_E);
+        robot.keyPress(KeyEvent.VK_S);
+        robot.keyPress(KeyEvent.VK_H);
+        robot.keyPress(KeyEvent.VK_9);
+        robot.keyPress(KeyEvent.VK_0);
+        robot.keyPress(KeyEvent.VK_CAPS_LOCK);
+        robot.keyPress(KeyEvent.VK_1);
+        robot.keyPress(KeyEvent.VK_CAPS_LOCK);        
+        robot.keyPress(KeyEvent.VK_ENTER);
+        // Press Enter
+        robot.keyPress(KeyEvent.VK_ENTER);        
+        // Release Enter
+        robot.keyRelease(KeyEvent.VK_ENTER);        
+
+        } catch (AWTException e) {
+        e.printStackTrace();
+        }    
     
-    try{
-    	driver.switchTo().frame("Authentication Required");
-    } catch (NoSuchFrameException e) {
+/*    try{
+    	//driver.switchTo().frame("Authentication Required");
+        // get currentwindow
+    	String currentWindow = driver.getWindowHandle();
+    	// Switch to JavaScript Alert window
+        Alert myAlert = driver.switchTo().alert();
+        System.out.println("Alert text is " + myAlert.getText());
+        driver.findElement(By.id("userId")).clear();
+        driver.findElement(By.id("userId")).sendKeys("haw");
+        driver.findElement(By.id("pass")).clear();
+        driver.findElement(By.id("pass")).sendKeys("Meshmesh90!");
+        // switch to frame and do stuff..
+        driver.switchTo().window(currentWindow); // switch back            
+    } catch (NoAlertPresentException e) {
+    //} catch (NoSuchFrameException e) {
     	//
-    	System.err.println("IndexOutOfBoundsException: " + e.getMessage());
-    }
-    
-    
-    driver.findElement(By.id("userId")).clear();
-    driver.findElement(By.id("userId")).sendKeys("haw");
-    driver.findElement(By.id("pass")).clear();
-    driver.findElement(By.id("pass")).sendKeys("Meshmesh90!");    
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    // switch to frame and do stuff..
-    //driver.switchTo().window(currentWindow); // switch back    
+    	System.err.println("NoAlertPresentException: " + e.getMessage());
+    }*/
+
   }
 
   @Test
